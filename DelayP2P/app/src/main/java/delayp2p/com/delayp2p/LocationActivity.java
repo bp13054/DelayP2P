475a24c1b49cbf1a2ca530197920bc791be09fae
bp13054 <bp13054@shibaura-it.ac.jp>
2017-04-26 20:46:04 +0900

    CSVに書き込めるようにした

--- DelayP2P/app/src/main/java/delayp2p/com/delayp2p/LocationActivity.java
+++ DelayP2P/app/src/main/java/delayp2p/com/delayp2p/LocationActivity.java
@@ -4,7 +4,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,10 +14,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationListener;
 import com.nifty.cloud.mb.core.NCMBObject;
@@ -24,7 +21,7 @@ import com.nifty.cloud.mb.core.NCMBObject;
 import java.util.List;
 
 
-public class LocationActivity extends AppCompatActivity implements LocationListener{
+public class LocationActivity extends AppCompatActivity implements LocationListener {
     final private static int LOCATION_UPDATE_COUNT = 5; //位置情報を5回取得したらNiftyに現在位置を登録．また，周辺ユーザ検索も実施
     final private static int MAX_UPDATE = 60;//位置情報を60回取得したら位置情報を取得することを終了する
     final private static int REQUEST_PERMISSION = 1000;
@@ -32,7 +29,8 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
     private P2PVer2 p2p; //SkyWayを使うクラス
     private int delayTime = 0;//初期値
     private String peerId = "unSetPeerId";//初期値
-    private int getLocationUpdateCount = 4;//位置情報のカウント回数を記録
+    private int getLocationUpdateCount = 4;//位置情報のカウント回数を記録 5回で初期化
+    private int LocationCounter = 0;
     private static List<NCMBObject> peerList;
     private Location location = new Location(""); //自身の位置情報を保存するのに用いる
     private TextView latLongTime;
@@ -45,22 +43,18 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         setContentView(R.layout.activity_location);
 
         nifty = new Nifty(this.getApplicationContext());
-        myLocation = new MyLocation(getApplicationContext(),this);
-
+        myLocation = new MyLocation(getApplicationContext(), this);
 
 
         Intent intent = getIntent();
         delayTime = intent.getIntExtra("Delay", 0);
         peerId = intent.getStringExtra("peerId");
         //Log.d("端末の時間誤差とID", "時間誤差:" + delayTime + "ID" + peerId);
-        p2p = new P2PVer2(this.getApplicationContext(), delayTime);
-
-
-
+        p2p = new P2PVer2(this.getApplicationContext(), delayTime, peerId);
 
 
         //Peerクラスに自身のidを登録し、Peerクラスを生成
-        p2p.createPeer(peerId);
+        p2p.createPeer();
 
 
         //実験を開始する
@@ -75,11 +69,6 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
             }
         });
     }
-
-
-
-
-
 
 
     /**
@@ -124,12 +113,18 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
             this.location.setLatitude(0);
             this.location.setLongitude(0);
         }
+        LocationCounter++;
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                latLongTime.setText("位置情報の取得回数:" + LocationCounter);
+            }
+        });
         getLocationUpdateCount++;//位置情報更新回数を+1する.....................................................................................................................................................................................................................................
         if (getLocationUpdateCount == LOCATION_UPDATE_COUNT) {//位置情報が5回更新されたタイミングでNiftyから周辺端末のリストを取得し，自身の現在位置情報も送信，その後，ほかのpeerに自身の位置情報を送信(最初は強制的に呼ぶ)
             getLocationUpdateCount = 0;//カウント回数を初期化
             System.gc();//メモリの初期化
-
-
 
 
             //////////////////////////////////////PeerListの更新//////////////////////////////
@@ -149,20 +144,17 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
             //////////////////////////////////////PeerListの更新//////////////////////////////
 
 
-
             nifty.setDataNifty(peerId, location);//ここでNiftyサーバに位置情報を送信
             p2p.connectPeer(peerList);
 
 
-
-
-            SetDate d = new SetDate();
-            String time = d.convertLong(location.getTime());
-            p2p.send(time);
+//            SetDate d = new SetDate();
+//            String time = d.convertLong(location.getTime());
+            p2p.send(location,"yes");
         } else {
-            SetDate d = new SetDate();
-            String time = d.convertLong(location.getTime());
-            p2p.send(time);
+//            SetDate d = new SetDate();
+//            String time = d.convertLong(location.getTime());
+            p2p.send(location,"");
         }
     }
 
@@ -171,11 +163,13 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
     @Override
     protected void onDestroy() {
         Log.d("確認", "onDestroy");
-        LocationServices.FusedLocationApi.removeLocationUpdates(myLocation.getGoogleApiClient(),this);
+        LocationServices.FusedLocationApi.removeLocationUpdates(myLocation.getGoogleApiClient(), this);
         nifty.deleteDataNifty();
         p2p.closeData();
         p2p.close();
         super.onDestroy();
+        p2p.getFileInput().fileClose();
+        p2p.getRecieveFileInput().fileClose();
     }
 
     protected void onStart() {
